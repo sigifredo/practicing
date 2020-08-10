d965b4c88b72e8d5fc8cd58652be3dcff12f8919
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2020-08-10 18:00:45 -0500

    Codigo indentado.

--- src/app/services/verbs-service.service.spec.ts
+++ src/app/services/verbs-service.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { VerbsServiceService } from './verbs-service.service';
 
 describe('VerbsServiceService', () => {
-  let service: VerbsServiceService;
+    let service: VerbsServiceService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(VerbsServiceService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(VerbsServiceService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
--- src/app/services/verbs-service.service.ts
+++ src/app/services/verbs-service.service.ts
@@ -1,16 +1,10 @@
 import { Injectable } from '@angular/core';
 import { VerbModel } from '../models/verb.model';
 import { BaseService } from './base-service.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-const httpOptions = {
-    headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-    })
-};
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class VerbsServiceService extends BaseService<VerbModel> {
 
