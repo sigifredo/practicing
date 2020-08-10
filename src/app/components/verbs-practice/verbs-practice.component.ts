0ca5ae6a076e3a6a844a987f1b93ef4cb15597d0
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2020-08-10 18:40:37 -0500

    Implementamos la práctica de los verbos.

--- src/app/components/verbs-practice/verbs-practice.component.ts
+++ src/app/components/verbs-practice/verbs-practice.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { VerbsService } from 'src/app/services/verbs.service';
+import { VerbModel } from '../../models/verb.model';
 
 @Component({
     selector: 'app-verbs-practice',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerbsPracticeComponent implements OnInit {
 
-    constructor() { }
+    private verbs:VerbModel[] = [];
+    message = { class: 'message', value: '' };
+    option = { present: '', past: '', pastparticiple: '' };
+    formResponse = { past: '', pastparticiple: '' };
+
+    constructor(private verbsService: VerbsService) { }
 
     ngOnInit(): void {
+        this.verbsService.getAll().subscribe(data => {
+            this.verbs = data;
+            this.getNextQuestion();
+        });
+    }
+
+    checkAnswers(past:string, pastparticiple:string) {
+        if (past.toLowerCase() == this.option.past && pastparticiple.toLowerCase() == this.option.pastparticiple) {
+            this.message.class = 'message';
+            this.message.value = 'Respuesta correcta';
+            this.getNextQuestion();
+        }
+        else {
+            this.message.class = 'error';
+            this.message.value = 'Respuesta incorrecta';
+        }
+        this.formResponse.past = '';
+        this.formResponse.pastparticiple = '';
+    }
+
+    getNextQuestion():void {
+        let i = Math.floor(Math.random()*this.verbs.length);
+
+        if (this.option.present == this.verbs[i].present) {
+            this.getNextQuestion();
+        }
+        else {
+            this.option.present = this.verbs[i].present;
+            this.option.past = this.verbs[i].past;
+            this.option.pastparticiple = this.verbs[i].pastparticiple;
+        }
     }
 
 }
